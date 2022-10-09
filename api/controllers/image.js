5cae325a4474ac9e5bb205f8093cebec2b61cec6
NizarAffess <nizaraffess@gmail.com>
2022-10-09 23:07:27 +0100

    api: add Imagee crud controllers

--- api/controllers/image.js
+++ api/controllers/image.js
@@ -17,5 +17,59 @@ const createImage = async (req, res) => {
   }
 };
 
+const updateImage = async (req, res) => {
+  try {
+    const image = await Image.findById(req.params.id);
+    if (!image) {
+      res.status(400).json({ message: "Image not found" });
+    }
+    const updatedImage = await Image.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+      }
+    );
+    res.status(200).json(updatedImage);
+  } catch (error) {
+    console.log("Error while updating image: ", error);
+    res.status(500).json(error);
+  }
+};
 
-module.exports = { createImage, getCreateRoute };
+const deleteImage = async (req, res) => {
+  try {
+    const image = await Image.findById(req.params.id);
+    if (!image) {
+      res.status(400).json({ message: "Image not found" });
+    }
+    const deletedImage = await Image.findByIdAndDelete(req.params.id);
+    res
+      .status(200)
+      .json({ message: "Image successfully deleted", id: deletedImage._id });
+  } catch (error) {
+    console.log("Error while deleting image: ", error);
+    res.status(500).json(error);
+  }
+};
+
+const getImage = async (req, res) => {
+  try {
+    const image = await Image.findById(req.params.id);
+    if (!image) {
+      res.status(400).json({ message: "Image not found" });
+    }
+    res.status(200).json(image);
+  } catch (error) {
+    console.log("Error while getting image: ", error);
+    res.status(500).json(error);
+  }
+};
+
+module.exports = {
+  createImage,
+  getCreateRoute,
+  updateImage,
+  deleteImage,
+  getImage,
+};
--- api/routes/image.js
+++ api/routes/image.js
@@ -1,8 +1,17 @@
-const { createImage, getCreateRoute } = require("../controllers/image");
+const {
+  createImage,
+  getCreateRoute,
+  updateImage,
+  deleteImage,
+  getImage,
+} = require("../controllers/image");
 
 const router = require("express").Router();
 
 router.get("/", getCreateRoute);
+router.get("/image/:id", getImage);
 router.post("/image/create", createImage);
+router.put("/image/:id", updateImage);
+router.delete("/image/:id", deleteImage);
 
 module.exports = router;
