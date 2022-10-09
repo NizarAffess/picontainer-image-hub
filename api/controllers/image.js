6504cf8032f7595a663f21c6a38d44aa9851e1be
NizarAffess <nizaraffess@gmail.com>
2022-10-09 23:07:01 +0100

    api: remove express-async-handler

--- api/controllers/image.js
+++ api/controllers/image.js
@@ -1,17 +1,21 @@
-const asyncHandler = require("express-async-handler");
 const Image = require("../models/image");
 
 const getCreateRoute = (req, res) => {
   res.status(200).json({ message: "Create image page" });
 };
 
-const createImage = asyncHandler(async (req, res) => {
-  console.log("LINE 9: ", req.body);
-  if (!req.body.title) {
-    res.status(400).json({ message: "Please add an image title" });
+const createImage = async (req, res) => {
+  try {
+    if (!req.body.title) {
+      res.status(400).json({ message: "Please add an image title" });
+    }
+    const image = await Image.create(req.body);
+    res.status(200).json(image);
+  } catch (error) {
+    console.log("Error while creating image: ", error);
+    res.status(500).json(error);
   }
-  const image = await Image.create(req.body);
-  res.status(200).json(image);
-});
+};
+
 
 module.exports = { createImage, getCreateRoute };
