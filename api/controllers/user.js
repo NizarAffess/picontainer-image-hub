67fb342fe432092dd6f2daf029d79ee396c779e5
NizarAffess <nizaraffess@gmail.com>
2022-10-10 15:12:50 +0100

    api: add loginUser controller

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -17,7 +17,7 @@ const registerUser = async (req, res) => {
     if (user) {
       const { password, ...rest } = user._doc;
       res
-        .status(200)
+        .status(201)
         .json({ message: `${user.username} successfully created`, rest });
     }
   } catch (error) {
@@ -26,4 +26,23 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    const { password, ...rest } = user._doc;
+    const comparedPassword = await bcrypt.compare(req.body.password, password);
+    if (user && comparedPassword) {
+      res
+        .status(200)
+        .json({ message: `${user.username} Successfully logged in`, rest });
+    } else {
+      res.status(400).json({ message: "Wrong credentials" });
+    }
+  } catch (error) {
+    console.log("Error while logging in: ", error);
+    res.status(500).json(error);
+  }
+};
+
+module.exports = { registerUser, loginUser };
