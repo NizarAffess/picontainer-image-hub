4bf517a9349349c3a91ad9420574211c7838e5bf
NizarAffess <nizaraffess@gmail.com>
2022-10-10 12:02:29 +0100

    api: add userID ref to Goal model

--- api/models/image.js
+++ api/models/image.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ImageSchema = new Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
   title: { type: String, required: true },
   description: { type: String },
   url: { type: String },
