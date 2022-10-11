582f4d243e2b178c7dfd6e7ed9ef4d4c830e83de
NizarAffess <nizaraffess@gmail.com>
2022-10-11 17:20:17 +0100

    api: decrease token expiration date

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -74,7 +74,7 @@ const getProfile = async (req, res) => {
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
+    expiresIn: "2d",
   });
 };
 
