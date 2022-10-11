bc2fdcb5efea846518d65a81d6cfb89f42e78ea4
NizarAffess <nizaraffess@gmail.com>
2022-10-11 17:19:01 +0100

    api: protect user profile route

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,8 +1,10 @@
-const { registerUser, loginUser } = require("../controllers/user");
+const { registerUser, loginUser, getProfile } = require("../controllers/user");
+const { protect } = require("../middleware/auth");
 
 const router = require("express").Router();
 
 router.post("/user/register", registerUser);
 router.post("/user/login", loginUser);
+router.get("/user/profile", protect, getProfile);
 
 module.exports = router;
