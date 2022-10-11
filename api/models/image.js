d6308a95a8a1e577e7b256b596ce6339d4b1e2ce
NizarAffess <nizaraffess@gmail.com>
2022-10-11 21:48:15 +0100

    api: add timestamps to Image Schema

--- api/models/image.js
+++ api/models/image.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ImageSchema = new Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
-  title: { type: String, required: true },
-  description: { type: String },
-  url: { type: String },
-});
+const ImageSchema = new Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
+    title: { type: String, required: true },
+    description: { type: String },
+    url: { type: String },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Image", ImageSchema);
