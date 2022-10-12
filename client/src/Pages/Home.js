2014104293b63b9a0de2135e5c349141dd08c399
NizarAffess <nizaraffess@gmail.com>
2022-10-12 09:31:49 +0100

    client: add Home page

---
+++ client/src/Pages/Home.js
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import { Container } from "@mui/material/";
+
+const Home = () => {
+  return (
+    <Container>
+      <Box>
+        <h1>Home page</h1>
+      </Box>
+    </Container>
+  );
+};
+
+export default Home;
