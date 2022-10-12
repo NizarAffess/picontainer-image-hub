0f75182db159de737527a856f26dd8fb337b9db3
NizarAffess <nizaraffess@gmail.com>
2022-10-12 08:56:47 +0100

    client: add Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h3>React Redux App</h3>
+      <Router>
+        <Navbar />
+      </Router>
     </div>
   );
 }
---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,42 @@
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+
+const pages = [
+  { title: "Home", endpoint: "/" },
+  { title: "Upload", endpoint: "/image/upload" },
+  { title: "Images", endpoint: "/images" },
+];
+
+const Navbar = () => {
+  return (
+    <Container
+      position="static"
+      sx={{
+        bgcolor: "text.primary",
+        textAlign: "center",
+        maxWidth: "100vw !important",
+      }}
+    >
+      <Box>
+        {pages.map((page) => (
+          <Link key={page.title} to={page.endpoint}>
+            <Button
+              sx={{
+                color: "white",
+                fontSize: "16px",
+                my: 1,
+                mx: 1,
+                textTransform: "capitalize",
+              }}
+            >
+              {page.title}
+            </Button>
+          </Link>
+        ))}
+      </Box>
+    </Container>
+  );
+};
+export default Navbar;
