27b6472a57aaac27945b51e6535295855023e712
NizarAffess <nizaraffess@gmail.com>
2022-10-12 16:53:48 +0100

    client: add authService

---
+++ client/src/features/auth/authService.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const registerUser = async (userData) => {
+  try {
+    const response = await axios.post(
+      "http://localhost:8083/api/user/register",
+      userData
+    );
+    if (response.data) {
+      // axios returns a response in an object called data
+      localStorage.setItem("user", JSON.stringify(response.data));
+    }
+    // const json = await response.json();
+    console.log("json response: ", response.data);
+    return response.data;
+  } catch (error) {
+    console.log("Error while creating user: ", error);
+  }
+};
+
+const authService = {
+  registerUser,
+};
+export default authService;
