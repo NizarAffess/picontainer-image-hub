79d1e6d9329a6e549390488ef20bde01286faea1
NizarAffess <nizaraffess@gmail.com>
2022-10-15 17:28:54 +0100

    api: minor auth cleanup

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -10,7 +10,6 @@ const protect = async (req, res, next) => {
     try {
       token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      console.log("LINE 12 DECODED: ", decoded);
       req.user = await User.findById(decoded.id);
       next();
     } catch (error) {
