afdd819e7b9ce04822e7477ceb55b159283e5ad3
NizarAffess <nizaraffess@gmail.com>
2022-10-16 09:05:59 +0100

    client: render imagesData in Images page

--- client/src/Pages/Images.js
+++ client/src/Pages/Images.js
@@ -1,10 +1,51 @@
-import { Container, Typography } from "@mui/material";
+import { ImageList, ImageListItem, ImageListItemBar } from "@mui/material";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import Spinner from "../Components/Spinner";
+import { getImages, reset } from "../features/images/imageSlice";
 
 const Images = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { user } = useSelector((state) => state.auth);
+  const { images, isError, isLoading, message } = useSelector(
+    (state) => state.images
+  );
+
+  useEffect(() => {
+    if (isError) {
+      console.log(message);
+    }
+    if (!user) {
+      navigate("/login");
+    }
+    dispatch(getImages());
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [isError, user, message, dispatch, navigate]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return (
-    <Container>
-      <Typography variant="h3">Images list</Typography>
-    </Container>
+    <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+      {images?.map((image) => (
+        <ImageListItem key={image.id}>
+          <img
+            src={`${image.url}?w=164&h=164&fit=crop&auto=format`}
+            srcSet={`${image.url}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+            alt={image.title}
+            loading="lazy"
+          />
+          <ImageListItemBar title={image.title} position="below" />
+        </ImageListItem>
+      ))}
+    </ImageList>
   );
 };
 
