e6824463287c408a86a67935255a11a1a8688d94
NizarAffess <nizaraffess@gmail.com>
2022-10-17 11:55:46 +0100

    client: add getImage slice & service

--- client/src/features/images/imageSlice.js
+++ client/src/features/images/imageSlice.js
@@ -40,6 +40,19 @@ const getImages = createAsyncThunk("/images", async (_, thunkAPI) => {
   }
 });
 
+const getImage = createAsyncThunk("/image/:id", async (id, thunkAPI) => {
+  try {
+    const { token } = thunkAPI.getState().auth.user.user;
+    return await imagesService.getImage(token, id);
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+    return thunkAPI.rejectWithValue(message);
+  }
+});
+
 const imageSlice = createSlice({
   name: "image",
   initialState,
@@ -73,10 +86,23 @@ const imageSlice = createSlice({
         state.isSuccess = false;
         state.isError = true;
         state.message = action.payload;
+      })
+      .addCase(getImage.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getImage.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.images = action.payload;
+      })
+      .addCase(getImage.rejected, (state, action) => {
+        state.isSuccess = false;
+        state.isError = true;
+        state.message = action.payload;
       });
   },
 });
 
 export const { reset } = imageSlice.actions;
-export { createImage, getImages };
+export { createImage, getImages, getImage };
 export default imageSlice.reducer;
