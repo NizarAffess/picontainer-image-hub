6e05366bb6bcdb2ff9efb4288f11bd6525cfe2cd
NizarAffess <nizaraffess@gmail.com>
2022-10-18 11:49:17 +0100

    client: restyle Sign component

--- client/src/Components/Sign.js
+++ client/src/Components/Sign.js
@@ -1,23 +1,24 @@
-import { Button } from "@mui/material";
+import { Button, List } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const Sign = () => {
   return (
-    <>
-      <Link to="/register">
+    <List sx={{ display: "inline", position: "absolute", top: -8, right: 15 }}>
+      <Link style={{ textDecoration: "none" }} to="/register">
         <Button
           sx={{
             color: "white",
             fontSize: "16px",
-            my: 1,
+            bgColor: "red",
             mx: 1,
             textTransform: "capitalize",
           }}
+          variant="contained"
         >
           Sign up
         </Button>
       </Link>
-      <Link to="/login">
+      <Link style={{ textDecoration: "none" }} to="/login">
         <Button
           sx={{
             color: "white",
@@ -25,12 +26,14 @@ const Sign = () => {
             my: 1,
             mx: 1,
             textTransform: "capitalize",
+            border: "1px solid #fff",
           }}
+          variant="outlined"
         >
           Sign in
         </Button>
       </Link>
-    </>
+    </List>
   );
 };
 
