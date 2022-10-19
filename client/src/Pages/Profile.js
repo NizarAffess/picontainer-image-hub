77cbe820f28a5c49bc61b21714d61df0bd6182bd
NizarAffess <nizaraffess@gmail.com>
2022-10-19 17:21:17 +0100

    client: add Avatar preview to Profile

--- client/src/Pages/Profile.js
+++ client/src/Pages/Profile.js
@@ -1,5 +1,12 @@
-import { Avatar, Box, Container, Grid, Typography } from "@mui/material";
-import { useEffect } from "react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { getUserProfile } from "../features/profile/profileSlice";
@@ -12,6 +19,22 @@ const Profile = () => {
   const { profile, isError, isLoading, message } = useSelector(
     (state) => state.profile
   );
+
+  const [file, setFile] = useState(null);
+  const [preview, setPreview] = useState(null);
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  useEffect(() => {
+    if (!file) {
+      setPreview("");
+      return;
+    }
+    const objectUrl = URL.createObjectURL(file);
+    setPreview(objectUrl);
+    return () => URL.revokeObjectURL(objectUrl);
+  }, [file]);
 
   useEffect(() => {
     if (isError) {
@@ -47,18 +70,51 @@ const Profile = () => {
       {profile ? (
         <Grid container rowSpacing={2} columnSpacing={3} sx={{ m: 2 }}>
           <Grid item textAlign="center">
-            <Avatar
-              sx={{ width: 200, height: 200 }}
-              src={profile.photo}
-              alt="profile"
-              loading="lazy"
-            />
+            {profile.photo ? (
+              <Avatar
+                sx={{ width: 200, height: 200 }}
+                src={profile.photo}
+                alt="profile"
+                loading="lazy"
+              />
+            ) : (
+              <Box sx={{ position: "relative" }}>
+                <Avatar
+                  sx={{ width: 200, height: 200 }}
+                  src={file ? preview : ""}
+                  alt="profile"
+                  loading="lazy"
+                />
+                <Button
+                  sx={{
+                    bgcolor: "text.primary",
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    display: file ? "none" : "block",
+                  }}
+                  variant="contained"
+                  component="label"
+                >
+                  {/* <UploadFile sx={{ mr: 1 }} /> */}
+                  Upload
+                  <input
+                    hidden
+                    onChange={handleFileChange}
+                    accept="image/*"
+                    type="file"
+                    required
+                  />
+                </Button>
+              </Box>
+            )}
             <Typography variant="h5" sx={{ my: 1 }}>
               {profile.username}
             </Typography>
           </Grid>
           <Grid item sx={{ mx: 4, maxWidth: "450px" }}>
-            <Typography component="p" sx={{ mb: 1 }}>
+            <Typography component="div" sx={{ mb: 1 }}>
               <Typography variant="h6">About</Typography>
               {profile.bio}
             </Typography>
