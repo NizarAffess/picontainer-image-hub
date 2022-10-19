db1099e798917bccc0ef324f8c654bb68861da87
NizarAffess <nizaraffess@gmail.com>
2022-10-19 12:02:52 +0100

    client: improve Profile page layout

--- client/src/Pages/Profile.js
+++ client/src/Pages/Profile.js
@@ -1,4 +1,4 @@
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Avatar, Box, Container, Grid, Typography } from "@mui/material";
 
 const Profile = () => {
   return (
@@ -17,18 +17,29 @@ const Profile = () => {
           loading="lazy"
         />
       </Box>
-      <Grid container spacing={2}>
-        <Grid item>
-          <img
+      <Grid container rowSpacing={2} columnSpacing={3} sx={{ m: 2 }}>
+        <Grid item textAlign="center">
+          <Avatar
+            sx={{ width: 200, height: 200 }}
             src="/test-assets/user-photo-1.jpg"
             alt="profile"
             loading="lazy"
           />
-          <Typography component="h3">username</Typography>
+          <Typography variant="h5" sx={{ my: 1 }}>
+            username
+          </Typography>
         </Grid>
-        <Grid item>
-          <Typography component="p">Biography</Typography>
-          <Typography component="p">Address</Typography>
+        <Grid item sx={{ mx: 4, maxWidth: "450px" }}>
+          <Typography component="p" sx={{ mb: 1 }}>
+            <Typography variant="h6">About</Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            odit dolore, perferendis praesentium tempore fugiat voluptate
+            assumenda esse quis debitis.
+          </Typography>
+          <Typography variant="h6">Address</Typography>
+          <Typography component="p">
+            CityVille, downtown, CountryLand
+          </Typography>
         </Grid>
       </Grid>
     </Container>
