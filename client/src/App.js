da878f7de74a9004b219885b60f83c60126b8e30
NizarAffess <nizaraffess@gmail.com>
2022-10-22 22:08:13 +0100

    client: split UploadForm component

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar";
-import UploadForm from "./Components/UploadForm";
 import Account from "./Pages/Account";
 import Home from "./Pages/Home";
 import Image from "./Pages/Image";
@@ -10,6 +9,7 @@ import Images from "./Pages/Images";
 import Profile from "./Pages/Profile";
 import SignIn from "./Pages/SignIn";
 import SignUp from "./Pages/SignUp";
+import UploadImage from "./Pages/UploadImage";
 
 function App() {
   return (
@@ -20,7 +20,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/images" element={<Images />} />
           <Route path="image/:id" element={<Image />} />
-          <Route path="/image/upload" element={<UploadForm />} />
+          <Route path="/image/upload" element={<UploadImage />} />
           <Route path="/register" element={<SignUp />} />
           <Route path="/login" element={<SignIn />} />
           <Route path="/user/profile" element={<Profile />} />
--- client/src/Components/UploadForm.js
+++ client/src/Components/UploadForm.js
@@ -3,67 +3,8 @@ import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { createImage, reset } from "../features/images/imageSlice";
-import Spinner from "./Spinner";
-import { useNavigate } from "react-router-dom";
 
 const UploadForm = (props) => {
-  const [file, setFile] = useState(null);
-  const [formData, setFormData] = useState({
-    title: "",
-    description: "",
-  });
-  const { title, description } = formData;
-
-  const { user } = useSelector((state) => state.auth);
-  const { isError, isLoading, isSuccess, message } = useSelector(
-    (state) => state.images
-  );
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
-  };
-
-  const submitImage = async (e) => {
-    e.preventDefault();
-    const imageData = new FormData();
-    imageData.append("title", title);
-    imageData.append("description", description);
-    imageData.append("url", file);
-    dispatch(createImage(imageData));
-  };
-
-  useEffect(() => {
-    if (isError) {
-      console.log("SNACKBAR: Error while creating image");
-    }
-
-    if (!user) {
-      navigate("/login");
-    }
-
-    if (isSuccess) {
-      console.log("SNACKBAR: Image successfully created");
-      navigate("/images", { state: { open: true, message } });
-    }
-    dispatch(reset());
-  }, [isError, isSuccess, user, dispatch, navigate, message]);
-
-  if (isLoading) {
-    return <Spinner />;
-  }
-
   return (
     <Container component="main" maxWidth="xs">
       <Box
@@ -77,25 +18,30 @@ const UploadForm = (props) => {
         <Typography component="h1" variant="h5">
           Upload New Image
         </Typography>
-        <Box component="form" onSubmit={submitImage} noValidate sx={{ mt: 1 }}>
+        <Box
+          component="form"
+          onSubmit={props.submitImage}
+          noValidate
+          sx={{ mt: 1 }}
+        >
           <TextField
             margin="normal"
             required
             fullWidth
-            value={formData?.title || ""}
+            value={props.formData?.title || ""}
             label="Title"
             name="title"
-            onChange={handleChange}
+            onChange={props.handleChange}
           />
           <TextField
             margin="normal"
             fullWidth
-            value={formData?.description || ""}
+            value={props.formData?.description || ""}
             multiline
             rows={3}
             label="Description"
             name="description"
-            onChange={handleChange}
+            onChange={props.handleChange}
           />
           <Button
             sx={{ bgcolor: "text.primary", mt: 2, p: 1 }}
@@ -107,7 +53,7 @@ const UploadForm = (props) => {
             Upload Image
             <input
               hidden
-              onChange={handleFileChange}
+              onChange={props.handleFileChange}
               accept="image/*"
               type="file"
               required
@@ -119,11 +65,12 @@ const UploadForm = (props) => {
             variant="contained"
             sx={{ bgcolor: "text.primary", mt: 3, mb: 2 }}
           >
-            Submit
+            {props.children}
           </Button>
         </Box>
       </Box>
     </Container>
   );
 };
+
 export default UploadForm;
