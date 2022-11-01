ffcdb28fe2988bca84ccc4c2085d0fc8986bee03
NizarAffess <nizaraffess@gmail.com>
2022-11-01 23:06:43 +0100

    client: minor change to test railway

--- client/src/Components/Gallery.js
+++ client/src/Components/Gallery.js
@@ -3,12 +3,12 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import Image from "./Image";
 
-const Gallery = ({ images, saveItem, isSaved }) => {
+const Gallery = (props) => {
   const { saved } = useSelector((state) => state.profile.profile);
 
   return (
     <ImageList sx={{ margin: "2em" }} cols={3}>
-      {images?.map((image) => (
+      {props.images?.map((image) => (
         <Box key={image._id} className="image">
           <Link to={`/image/${image._id}`} className="image-item">
             <Image image={image} />
@@ -16,11 +16,11 @@ const Gallery = ({ images, saveItem, isSaved }) => {
           <IconButton
             aria-label={`save ${image.title}`}
             className="image-title image-save"
-            onClick={() => saveItem(image._id)}
+            onClick={() => props.saveItem(image._id)}
           >
             {/* <BookmarkBorderIcon aria-label="save" /> 
             <BookmarkIcon aria-label="unsave" /> */}
-            {saved && isSaved(image._id) ? "Unsave" : "Save"}
+            {saved && props.isSaved(image._id) ? "Unsave" : "Save"}
           </IconButton>
         </Box>
       ))}
