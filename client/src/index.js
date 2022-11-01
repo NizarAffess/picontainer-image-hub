306782d20812a6d5f80f64775ce29a3d995c3057
NizarAffess <nizaraffess@gmail.com>
2022-11-01 21:52:32 +0100

    client: set up frontend deployment

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 root.render(
   <React.StrictMode>
