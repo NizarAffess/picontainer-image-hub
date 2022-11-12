2a83060dc5aaf7975edcfacdb15d8fe1f595f3f8
NizarAffess <nizaraffess@gmail.com>
2022-11-12 14:28:23 +0100

    client: add Home page content

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,8 +1,8 @@
-import { Box, IconButton, Snackbar } from "@mui/material";
+import { Box, Button, Grid, IconButton, Snackbar } from "@mui/material";
 import { Container } from "@mui/material/";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Home = () => {
   const { message } = useSelector((state) =>
@@ -37,7 +37,6 @@ const Home = () => {
   return (
     <Container>
       <Box>
-        <h1>Home page</h1>
         <Snackbar
           open={snack}
           autoHideDuration={5000}
@@ -45,6 +44,35 @@ const Home = () => {
           message={message}
           action={action}
         />
+        <Grid
+          container
+          spacing={2}
+          style={{ alignItems: "center", marginTop: "2em" }}
+        >
+          <Grid item xs={12} md={6}>
+            <img
+              src="https://res.cloudinary.com/dru9x9rey/image/upload/v1668249880/projects/undraw_inspiration_re_ivlv_opfpfn.svg"
+              alt="inspiration"
+              style={{ width: "80%" }}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <h2 style={{ width: "250px" }}>
+              Welcome to the Land of Photographers
+            </h2>
+            <p style={{ width: "250px", margin: "1em 0" }}>
+              Join a creative community and start contributing
+            </p>
+            <Link to="/login" style={{ textDecoration: "none" }}>
+              <Button
+                variant="contained"
+                sx={{ bgcolor: "#00B0FF", textTransform: "capitalize" }}
+              >
+                Get started
+              </Button>
+            </Link>
+          </Grid>
+        </Grid>
       </Box>
     </Container>
   );
